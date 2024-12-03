b3b945c5b9611e8eaf265c13ca34c22e7fe117e1
BrycenP <perezb3@mail.uc.edu>
2024-12-02 19:47:00 -0500

    NIO should be working

--- src/FileInspector.java
+++ src/FileInspector.java
@@ -11,7 +11,7 @@ import static java.nio.file.StandardOpenOption.CREATE;
  *
  * Use the thread safe NIO IO library to read a file
  */
-public class NIOReadTextFile
+public class FileInspector
 {
 
     /**
@@ -22,43 +22,40 @@ public class NIOReadTextFile
         JFileChooser chooser = new JFileChooser();
         File selectedFile;
         String rec = "";
+        chooser.setCurrentDirectory(new File(System.getProperty("user.dir")+"/src"));
 
         try {
-            // uses a fixed known path:
-            //  Path file = Paths.get("c:\\My Documents\\data.txt");
-
-            // use the toolkit to get the current working directory of the IDE
-            // Not sure if the toolkit is thread safe...
             File workingDirectory = new File(System.getProperty("user.dir"));
 
-            // Typiacally, we want the user to pick the file so we use a file chooser
-            // kind of ugly code to make the chooser work with NIO.
-            // Because the chooser is part of Swing it should be thread safe.
             chooser.setCurrentDirectory(workingDirectory);
-            // Using the chooser adds some complexity to the code.
-            // we have to code the complete program within the conditional return of
-            // the filechooser because the user can close it without picking a file
+            chooser.rescanCurrentDirectory();
 
             if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
                 selectedFile = chooser.getSelectedFile();
                 Path file = selectedFile.toPath();
-                // Typical java pattern of inherited classes
-                // we wrap a BufferedWriter around a lower level BufferedOutputStream
+
                 InputStream in =
-                        new BufferedInputStream(Files.newInputStream(file, CREATE));
+                        new BufferedInputStream(Files.newInputStream(file));
                 BufferedReader reader =
                         new BufferedReader(new InputStreamReader(in));
 
-                // Finally we can read the file LOL!
-                int line = 0;
+                int lines = 0;
+                int words = 0;
+                int characters = 0;
                 while (reader.ready()) {
                     rec = reader.readLine();
-                    line++;
-                    // echo to screen
-                    System.out.printf("\nLine %4d %-60s ", line, rec);
+                    lines++;
+                    words += rec.split("\\s").length;
+                    characters += rec.length();
+                    System.out.printf("\nLine %4d %-60s ", lines, rec);
                 }
                 reader.close(); // must close the file to seal it and flush buffer
                 System.out.println("\n\nData file read!");
+                System.out.println("Name: " + selectedFile.getName());
+                System.out.println("Word count: " +words);
+                System.out.println("Line count: " +lines);
+                System.out.println("Character count: " + characters);
+
 
 
             } else  // User closed the chooser without selecting a file
